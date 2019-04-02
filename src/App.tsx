03aab063e8a7010e53881752561baaf28a37e62b
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2019-04-03 01:35:01 +0900

    feat: global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,11 @@
 import React from "react";
 
-import logo from "./logo.svg";
-import "./App.css";
+import { Container, Content } from "./components/Layout";
 
 const App: React.FC = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} alt="logo" className="App-logo" />
-      <p>
-        Edit
-        <code>src/App.tsx</code>
-        and save to reload.
-      </p>
-      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-        Learn React
-      </a>
-    </header>
-  </div>
+  <Container>
+    <Content>Hello, World</Content>
+  </Container>
 );
 
 export default App;
---
+++ src/components/Layout.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.25);
+`;
+
+const Content = styled.div`
+  flex: 1;
+`;
+
+export { Container, Content };
