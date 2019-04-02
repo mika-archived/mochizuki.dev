e3231925fb668b089a322ee897e0dca688ec21d1
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2019-04-03 01:38:52 +0900

    feat(footer): add a footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 
+import Footer from "./components/Footer";
 import { Container, Content } from "./components/Layout";
 
 const App: React.FC = () => (
   <Container>
     <Content>Hello, World</Content>
+    <Footer />
   </Container>
 );
 
