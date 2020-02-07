5ba8da37fe3328ab526c209833497854a20db610
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2020-02-07 21:59:43 +0900

    feat: loading animation while downloading react bundles

--- src/components/molecules/NowLoading/index.stories.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-
-import NowLoading from ".";
-
-storiesOf("molecules/NowLoading", module).add("Default", () => <NowLoading />);
--- src/main.tsx
+++ src/main.tsx
@@ -8,17 +8,33 @@ html, body {
   margin: 0;
 }
 
-#loading {
-  height: 100vh;
-  min-height: 100vh;
-  color: #fff;
-  background-color: #333;
+.rendering {
+  animation: fadeIn 1s; 
+}
+
+@keyframes fadeIn {
+  0% {
+    opacity: 0;
+  }
+
+  100% {
+    opacity: 1;
+  }
 }
 `;
+
+const App: React.FC = () => {
+  return (
+    <div className="rendering">
+      <div>Hello, World</div>
+    </div>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
     <GlobalStyle />
+    <App />
   </React.StrictMode>,
   document.querySelector("#app")
 );
