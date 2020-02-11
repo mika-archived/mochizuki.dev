3d22087c223232f285dc7bbd8e7eef2c22036200
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2020-02-11 21:25:59 +0900

    fix: use media query in container

--- src/components/atoms/Container/index.tsx
+++ src/components/atoms/Container/index.tsx
@@ -4,6 +4,10 @@ const Container = styled.div`
   width: 100%;
   max-width: 600px;
   margin: 0 auto;
+
+  @media screen and (max-width: 600px) {
+    width: 90%;
+  }
 `;
 
 export default Container;
